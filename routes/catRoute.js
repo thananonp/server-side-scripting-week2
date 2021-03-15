698688f6cbec2f5e152ca71339434c30fa8d322b
Thananon Pongsuwan <thenightraven7407@gmail.com>
2021-03-15 12:18:12 +0200

    5. Study MVC

--- routes/catRoute.js
+++ routes/catRoute.js
@@ -3,20 +3,25 @@
 const express = require('express');
 const router = express.Router();
 
+const catController = require('../controllers/catController');
 
-router.route('/cat')
-    .delete(function (req, res) {
-        res.send('With this endpoint you can delete cats.')
-    })
-    .post(function (req, res) {
-        res.send('With this endpoint you can add cats.')
-    })
-    .put(function (req, res) {
-        res.send('With this endpoint you can edit cats.')
-    })
+router.get('/cat', catController.cat_list_get);
 
-router.get('/cat/:id', function (req, res) {
-    res.send("You requested a cat whose id is " + req.params.id)
-    console.log("Cat " + req.params.id + " is requested")
-})
+module.exports = router;
+//
+// router.route('/cat')
+//     .delete(function (req, res) {
+//         res.send('With this endpoint you can delete cats.')
+//     })
+//     .post(function (req, res) {
+//         res.send('With this endpoint you can add cats.')
+//     })
+//     .put(function (req, res) {
+//         res.send('With this endpoint you can edit cats.')
+//     })
+//
+// router.get('/cat/:id', function (req, res) {
+//     res.send("You requested a cat whose id is " + req.params.id)
+//     console.log("Cat " + req.params.id + " is requested")
+// })
 module.exports = router
