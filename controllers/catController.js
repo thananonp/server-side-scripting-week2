df044e45f80d2352243c5fa26ad01e35e55ec1a2
Thananon Pongsuwan <thenightraven7407@gmail.com>
2021-03-15 12:27:11 +0200

    6. Using array.filter to select matching cat

--- controllers/catController.js
+++ controllers/catController.js
@@ -8,6 +8,12 @@ const cat_list_get = (req, res) => {
     res.json(cats);
 };
 
+const cat_get = (req, res) => {
+    let selectedCat = cats.filter((cat) => cat.id === req.params.id)
+    res.json(selectedCat)
+}
+
 module.exports = {
     cat_list_get,
+    cat_get
 };
--- routes/catRoute.js
+++ routes/catRoute.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const catController = require('../controllers/catController');
 
 router.get('/cat', catController.cat_list_get);
+router.get('/cat/:id', catController.cat_get);
 
 module.exports = router;
 //
